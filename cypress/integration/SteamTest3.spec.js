7e1de538dba6591ce9f2a8f3a7f7110ed77a4978
James Holmquist <JamesMHolmquist@gmail.com>
2022-02-17 13:46:55 +0100

    Added more steps to test 3 and fixed the name and description

--- cypress/integration/SteamTest3.spec.js
+++ cypress/integration/SteamTest3.spec.js
@@ -4,8 +4,8 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-describe("Test 1", () =>{
-    it("Tests Search and age gate", () =>{
+describe("Test 3", () =>{
+    it("Tests adding and removing to cart", () =>{
         cy.visit("https://www.steampowered.com/")
 
         cy.get("[id=store_nav_search_term]").type("Elden Ring")
@@ -17,7 +17,8 @@ describe("Test 1", () =>{
         cy.get("#btn_add_to_cart_432247").click()
         cy.url().should("include", "https://store.steampowered.com/cart/")
         cy.get("[id^=cart_row_]").should("include.text", "ELDEN RING")
-        
+        cy.get("[id^=cart_row_] > div > div.cart_item_price_container > div > a").click()
+        cy.get("body > div.responsive_page_frame.with_header > div.responsive_page_content > div.responsive_page_template_content > div.page_background > div.page_content > div.leftcol.cart_status_message_area > div.cart_status_message_ctn > div.cart_status_message").should("include.text", "Your item has been removed!")
 
     })
 })
