ea2615e07694ab0265d99fa07ec5388fda1b3d94
James Holmquist <JamesMHolmquist@gmail.com>
2022-02-17 12:02:14 +0100

    First test completed

---
+++ cypress/integration/SteamTest1.spec.js
@@ -0,0 +1,22 @@
+// TeamsTest1.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe("Test 1", () =>{
+    it("Tests Search and age gate", () =>{
+        cy.visit("https://www.steampowered.com/")
+
+        cy.get("[id=store_nav_search_term]").type("Elden Ring")
+        cy.get("#store_search_link > img").click()
+        cy.get("#search_resultsRows > a:nth-child(1) > div.col.search_capsule > img").click()
+        cy.get("#view_product_page_btn").click()
+        cy.get("body > div.newmodal > div.newmodal_content_border > div > div:nth-child(1)").should("include.text", "Please enter a valid date")
+        cy.get("body > div.newmodal > div.newmodal_content_border > div > div.newmodal_buttons > div").click()
+        cy.get("#ageYear").select("1993")
+        cy.get("#view_product_page_btn").click()
+        cy.url().should("include", "https://store.steampowered.com/app/1245620/ELDEN_RING/")
+
+    })
+})
